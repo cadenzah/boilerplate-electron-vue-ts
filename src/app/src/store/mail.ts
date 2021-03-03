56ad3787a9ba8c60175e0f85a76e11160ab7e170
cadenzah <cadenzah93@gmail.com>
2021-03-03 22:13:32 +0900

    added sample script to test IPC

--- src/app/src/store/mail.ts
+++ src/app/src/store/mail.ts
@@ -1,4 +1,5 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+import { ipcRenderer } from 'electron';
 
 @Module({ namespaced: true, name: 'mail' })
 export default class Mail extends VuexModule {
@@ -10,11 +11,13 @@ export default class Mail extends VuexModule {
 
   @Mutation
   addCounter(val: number): number {
+    ipcRenderer.send('test', { value: this.counter + val });
     return this.counter = this.counter + val;
   }
 
   @Mutation
   subCounter(val: number): number {
+    ipcRenderer.send('test', { value: this.counter - val });
     return this.counter = this.counter - val;
   }
 };
